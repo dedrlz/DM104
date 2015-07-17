d6e93cd7ab17053032c5c54beda3c67f6e765889
dedrlz <ded_ffa@hotmail.com>
2015-07-17 01:12:18 -0300

    Facebook like
    HTML link to Facebook API for page like

---
+++ loja/facelike.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml"
+xmlns:og="http://ogp.me/ns#"
+xmlns:fb="http://www.facebook.com/2008/fbml">
+
+<head>
+
+<title>Face like</title>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '818777294896950',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
+</head>
+<body bgcolor="#ffffff">
+  <div
+          class="fb-like"
+          data-share="true"
+          data-width="450"
+          data-show-faces="true">
+  </div>
+</body>
+<!-- InstanceEnd --></html>
